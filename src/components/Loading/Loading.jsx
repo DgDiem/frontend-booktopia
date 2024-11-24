d2098b6afa920af1abce1fbdd2c9fc7dff80aa06
LangPN <langpn.dev@gmail.com>
2024-11-24 16:58:56 +0700

    refactor(load): turn off loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { RotatingSquare } from "react-loader-spinner";
 
 const Loading = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
